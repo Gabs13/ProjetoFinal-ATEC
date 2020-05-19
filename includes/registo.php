c8011d3fe82fb4905593798fda6492972f6009bf
Gabs13 <gabriel.cosme.1999@gmail.com>
2020-05-19 23:48:55 +0100

    Alteração css e add pagina

---
+++ includes/registo.php
@@ -0,0 +1,29 @@
+<html>
+    <link rel="stylesheet" type="text/css" href="CSS/estiloRegisto.css">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald&display=swap" rel="stylesheet">
+
+    <header>
+        <div class="Logo"><img src="Imagens/Logo2.png"></div>
+    </header>
+    <body>
+
+    <div class=pai>
+
+        <div class="filho">
+            <div class="titulo">Register your new account</div>
+            <div class="borders border1"><input type="text" placeholder="Primeiro Nome"></div>
+            <div class="borders"><input type="text" placeholder="Ultimo Nome"></div>
+            <div class="borders"><input type="password" placeholder="Password"></div>
+            <div class="borders"><input type="text" placeholder="Confirmar Password"></div>
+            <div class="borders"><input type="number" placeholder="Telemovel"></div>
+            <div class="borders"><input type="checkbox" placeholder="Género"></div>
+            <div class="borders"><input type="email" placeholder="E-mail"></div>
+            <div class="borders"><input type="date" placeholder="Data de Nascimento"></div>
+            <div>
+                <button>Registar</button>
+            </div>
+        </div>
+
+    </div>
+    </body>
+</html>
