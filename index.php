7612dbc7397d21d68f0b05626fc794400ffdf15a
Gabs13 <gabriel.cosme.1999@gmail.com>
2020-05-20 10:27:37 +0100

    Alterações no registo

--- index.php
+++ index.php
@@ -12,13 +12,14 @@ require_once 'functions/functions.php';
     <link rel="stylesheet" type="text/css" href="css/estilo.css">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital@0;1&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=PT+Serif&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald&display=swap" rel="stylesheet">
     <script src="scrips.js"></script>
   </head>
   <body>
 
     <?php
-
-    include 'includes/header.php';
+    include 'includes/headerregisto.php';
+    include 'includes/registo.php';
 
     ?>
     <!-- teste -->
