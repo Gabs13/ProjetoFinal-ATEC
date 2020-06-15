d39fb43321b32d8529ec00dd210c38c8ea3ea17d
fabio3070 <fabiosantos966@gmail.com>
2020-06-15 20:02:37 +0100

    Mudanças chat 6/18/2020

--- includes/chat.php
+++ includes/chat.php
@@ -1,5 +1,6 @@
 <div class="chat_body">
     <div class="chat_users">
+    <div class="chat_users_settings">botao de escrever nova mensagem e settings</div>
         <div class="chat_users_display">
             <!--MENSAGEM DE UM UTILIZADOR-->
             <div class="chat_users_display_user">
@@ -11,12 +12,44 @@
             </div>
             <!--FINAL-->
         </div>
-        <div class="chat_users_settings">botao de escrever nova mensagem e settings</div>
+        
     </div>
     <div class="chat_display">
-        <div class="chat_display_user"></div>
+        <div class="chat_display_user">
+            <div class="chat_display_user_profile">
+                <div class="chat_display_user_img"><a><img src="Imagens/Icones/icons8-male-user-26.png"></a></div>
+                <div class="chat_display_user_name">Ruben do Tuning</div>
+            </div>
+        </div>
+
+        <div class="chat_display_messages">
+            <div class="chat_display_messages_ocupy">
+                <div class="chat_display_messages_display">
+                    <div class="chat_display_messages_nome">Armando Nunes</div>
+                    <div class="chat_display_messages_texto">Nao gostei do teu honda ruben, é roubado e deita fumo</div>
+                    <div class="chat_display_messages_hora">16:04</div>
+                </div>
+            </div>
+            
+            <div class="chat_display_messages_ocupy">
+                <div class="chat_display_messages_display_left">
+                    <div class="chat_display_messages_nome">Armando Nunes</div>
+                    <div class="chat_display_messages_texto">Nao gostei do teu honda ruben, é roubado e deita fumo</div>
+                    <div class="chat_display_messages_hora">16:04</div>
+                </div>
+            </div>
+        </div><!--FINAL DO DISPLAY DAS MENSAGENS-->
+
+        
+        
+
         <div class="chat_display_text">
-            <div class="chat_display_text_msg" ><input type="text" placeholder="Escreva a sua mensagem..."></div>
+            <div class="chat_display_text_msg" >
+                <input type="text" placeholder="Escreva a sua mensagem...">
+                <div class="chat_display_text_send">
+                    <i class="fas fa-paper-plane"></i>
+                </div>
+            </div>
             <div class="chat_display_text_send"></div>
         </div>
     </div>
