e86183e0c231a4a54044194d937c5de3121999bc
fabio3070 <fabiosantos966@gmail.com>
2020-06-17 09:20:13 +0100

    resultados de pesquisa coisas bonitas

--- includes/resultadoPesquisa.php
+++ includes/resultadoPesquisa.php
@@ -1,27 +1,29 @@
 <body>
     <div class="resultado_Pesquisa_total">
         <div class="resultadoPesquisa_back"><i class="fas fa-arrow-circle-left"></i></div>
-        <div class="resultadoPesquisa_body">          
+        <div class="resultadoPesquisa_body">  
+            <div class="resultadoPesquisa_body_label">Pesquisa:</div>        
             <div class="resultadosPesquisa_container">
                 <div class="resultadosPesquisa_container_img"><a><img src="Imagens/Icones/icons8-male-user-26.png"></a></div>
                 <div class="resultadosPesquisa_container_nome">dfbsudfbsdfsd</div>
-                <div class="resultadosPesquisa_container_chat"><i class="far fa-comment-dots"></i></div>
+                <div class="resultadosPesquisa_container_chat"><i class="fas fa-plus"></i></div>
             </div>
             <div class="resultadosPesquisa_container">
                 <div class="resultadosPesquisa_container_img"><a><img src="Imagens/Icones/icons8-male-user-26.png"></a></div>
                 <div class="resultadosPesquisa_container_nome">dfbsudfbsdfsd</div>
-                <div class="resultadosPesquisa_container_chat"><i class="fas fa-comment-dots"></i></div>
+                <div class="resultadosPesquisa_container_chat"><i class="fas fa-plus"></i></div>
             </div>
             <div class="resultadosPesquisa_container">
                 <div class="resultadosPesquisa_container_img"><a><img src="Imagens/Icones/icons8-male-user-26.png"></a></div>
                 <div class="resultadosPesquisa_container_nome">dfbsudfbsdfsd</div>
-                <div class="resultadosPesquisa_container_chat"><i class="fas fa-comment-alt"></i></div>
+                <div class="resultadosPesquisa_container_chat"><i class="fas fa-plus"></i></div>
             </div>
             <div class="resultadosPesquisa_container">
                 <div class="resultadosPesquisa_container_img"><a><img src="Imagens/Icones/icons8-male-user-26.png"></a></div>
                 <div class="resultadosPesquisa_container_nome">dfbsudfbsdfsd</div>
-                <div class="resultadosPesquisa_container_chat"><i class="fas fa-comment-alt"></i></div>
+                <div class="resultadosPesquisa_container_chat"><i class="fas fa-plus"></i></div>
             </div>
+            <div class="resultadoPesquisa_body_end"></div>
         
         </div>
     </div>
