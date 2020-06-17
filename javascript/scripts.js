14dc9c8f65cb89142192219ee7e4ab2706a007df
Gabs13 <gabriel.cosme.1999@gmail.com>
2020-06-17 08:31:48 +0100

    Alterações 17/06/2020

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -25,18 +25,24 @@ $(document).ready(function()
   //pesquisa de utilizadores no chat
   var clickSearchUser = document.getElementById('PesquisaNome');
   var dropdownResultados = document.getElementsByClassName('chat_resultados_pesquisa')[0];
-  
+
   /*funcao para aparecer resultados pesquisados*/
   clickSearchUser.onfocus = function()
   {
     dropdownResultados.style.display="block";
     dropdownResultados.style.transition= "1s";
+
+    $('#chat_user_settings_search').css('border-bottom-left-radius', '0px');
+    $('#chat_user_settings_search').css('border-bottom-right-radius', '0px');
+
   }
-  /*sair dos resultados pesquisados*/ 
+  /*sair dos resultados pesquisados*/
   clickSearchUser.onfocusout = function()
   {
     dropdownResultados.style.display="none";
     dropdownResultados.style.transition= "1s";
+
+    $('#chat_user_settings_search').css('border-radius', '8px');
   }
   $("#PesquisaNome").focusout(function(){
 
