8df79d4da39cf4c58c4f3bf21503fb27bc67a981
Gabs13 <gabriel.cosme.1999@gmail.com>
2020-06-23 03:24:38 +0100

    Estilos e JavaScript Chat 23/06/2020

--- javascript/scriptsmsgs.js
+++ javascript/scriptsmsgs.js
@@ -47,10 +47,15 @@ $(document).ready(function(){
 
 
 
-function limparMsgs()
+function limparMsgs(id)
 {
     $('#chat_display_messages').empty();
     start = 0;
+
+
+    $('#chat_display_user_name').load("functions/CarregarNomeChat.php", {id: id});
+
+    $('#chat_display').css('display', 'block');
 }
 
 function loadMsgs(id)
@@ -66,7 +71,7 @@ function loadMsgs(id)
       })
       $('#chat_display_messages').animate({scrollTop: $('#chat_display_messages')[0].scrollHeight});
     };
-
+    $("#chat_users_display").load("functions/CarregarMensagens.php");
     setTimeout(function() {loadMsgs(cid);}, 1000)
   });
 
@@ -75,7 +80,12 @@ function loadMsgs(id)
 
 function renderMessage(item)
 {
+  let today = new Date();
+  let diamsg = new Date(item.DataMsg);
   let time = new Date(item.DataMsg);
+
+  console.log(diamsg.getDay());
+
   if (time.getMinutes() < 10){
     time = time.getHours() + ':0' + time.getMinutes();
   }
@@ -84,10 +94,26 @@ function renderMessage(item)
   }
   if (item.UtilID == IDSessao)
   {
-    return '<div class="chat_display_messages_ocupy"> <div class="chat_display_messages_display"> <div class="chat_display_messages_nome">' + item.UtilPNome + ' ' + item.UtilUNome + '</div> <div class="chat_display_messages_texto">' + item.Mensagem + '</div> <div class="chat_display_messages_hora">' + time + '</div> </div> </div>';
+    if (diamsg.getDay() != today.getDay())
+    {
+      return '<div class="chat_display_messages_ocupy"> <div class="chat_display_messages_display"> <div class="chat_display_messages_nome">' + item.UtilPNome + ' ' + item.UtilUNome + '</div> <div class="chat_display_messages_texto">' + item.Mensagem + '</div> <div class="chat_display_messages_hora">' + (diamsg.getMonth() + 1) + '/' + diamsg.getDate() + ' ' + time + '</div> </div> </div>';
+    }
+    else
+    {
+      return '<div class="chat_display_messages_ocupy"> <div class="chat_display_messages_display"> <div class="chat_display_messages_nome">' + item.UtilPNome + ' ' + item.UtilUNome + '</div> <div class="chat_display_messages_texto">' + item.Mensagem + '</div> <div class="chat_display_messages_hora">' + time + '</div> </div> </div>';
+    }
+
   }
   else
   {
-    return '<div class="chat_display_messages_ocupy"> <div class="chat_display_messages_display_left"> <div class="chat_display_messages_nome">' + item.UtilPNome + ' ' + item.UtilUNome + '</div> <div class="chat_display_messages_texto">' + item.Mensagem + '</div> <div class="chat_display_messages_hora">' + time + '</div> </div> </div>' ;
+    if (diamsg.getDay() != today.getDay())
+    {
+      return '<div class="chat_display_messages_ocupy"> <div class="chat_display_messages_display_left"> <div class="chat_display_messages_nome">' + item.UtilPNome + ' ' + item.UtilUNome + '</div> <div class="chat_display_messages_texto">' + item.Mensagem + '</div> <div class="chat_display_messages_hora">' + (diamsg.getMonth() + 1) + '/' + diamsg.getDate() + ' ' + time + '</div> </div> </div>' ;
+    }
+    else
+    {
+      return '<div class="chat_display_messages_ocupy"> <div class="chat_display_messages_display_left"> <div class="chat_display_messages_nome">' + item.UtilPNome + ' ' + item.UtilUNome + '</div> <div class="chat_display_messages_texto">' + item.Mensagem + '</div> <div class="chat_display_messages_hora">' + time + '</div> </div> </div>' ;
+    }
+
   }
 }
