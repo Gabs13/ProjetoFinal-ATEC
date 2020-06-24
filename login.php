1a119d66e77fc4597ed6222e84c21ee2076ef8a0
fabio3070 <fabiosantos966@gmail.com>
2020-06-24 21:53:06 +0100

    novo perfil 06/24/2020

--- login.php
+++ login.php
@@ -47,7 +47,7 @@
       if(@!$_GET['pid'] && @!$_GET['uid'])
       {
         //include 'includes/criaposts.php';
-        include 'includes/home.php';
+        include 'includes/perfil_novo.php';
       }
       else
       {
